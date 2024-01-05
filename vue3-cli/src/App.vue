df804438744f4308fd76415ef3660d4882fe996c
jiheon2234 <jiheon2234@gmail.com>
2024-01-05 22:49:17 +0900

    single file componenet

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <app-header v-bind:appTitle="message" v-on:change="changeMethod" />
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader: AppHeader,
+  },
+  data() {
+    return {
+      message: "엡 헤더 컴포넌트",
+    };
+  },
+  methods: {
+    changeMethod() {
+      this.message = "변경됨";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
